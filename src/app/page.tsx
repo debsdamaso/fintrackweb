c1955937ae11d9cd40face9f8aada5bc1260ff63
debsdamaso <deboracert@gmail.com>
2024-04-11 10:10:36 -0300

    aula 1 - menu | tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { NavBar } from "@/components/NavBar"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <NavBar active="dashboard"/>
+      <h2>Dashboard</h2>
+      </main>
+    );
+}
